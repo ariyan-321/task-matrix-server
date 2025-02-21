c6156742fd3c53198a1b6c5fa81a8e960b82cdf2
Your Name <ariyaniphone14@gmail.com>
2025-02-21 08:22:42 +0600

    added delete route

--- index.js
+++ index.js
@@ -38,6 +38,13 @@ async function run() {
     })
 
 
+    app.delete("/tasks/:id",async(req,res)=>{
+        const id=req.params.id;
+        const query={_id: new Object(id)}
+        const result=await taskCollection.deleteOne(query);
+        res.send(result);
+    })
+
 
     
   } finally {
