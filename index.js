909444a724000bd0f0ecb46572256bc1461666c2
Your Name <ariyaniphone14@gmail.com>
2025-02-21 12:29:20 +0600

    added user info to database

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-const express = require('express');
-const cors = require('cors');
-const dotenv = require('dotenv');
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 dotenv.config();
 
-const port=process.env.PORT || 5000;
-const app=express();
-app.use(cors())
+const port = process.env.PORT || 5000;
+const app = express();
+app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@ariyan.mefyr.mongodb.net/?retryWrites=true&w=majority&appName=Ariyan`;
@@ -18,74 +17,75 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
+
 async function run() {
   try {
+    const db = client.db("Task-Matrix");
+    const taskCollection = db.collection("tasks");
+    const usersCollection = db.collection("users");
 
-    const db=client.db("Task-Matrix");
-    const taskCollection=db.collection("tasks");
-
-    app.post("/tasks",async(req,res)=>{
-        const data=req.body;
-        const result=await taskCollection.insertOne(data);
-        res.send(result);
-    })
-
-    app.get("/tasks",async(req,res)=>{
-        const result=await taskCollection.find().toArray();
-        res.send(result);
-    })
-
-
-    app.get("/tasks/:id",async(req,res)=>{
-        const id=req.params.id;
-        const query={_id:new ObjectId(id)}
-        const result=await taskCollection.findOne(query);
-        res.send(result);
-    })
+    app.post("/users", async (req, res) => {
+      const { userInfo } = req.body;
+      console.log(userInfo)
+      const query = { email: userInfo?.email };
+      const existingUser = await usersCollection.findOne(query);
+      if (existingUser) {
+        return res.send({
+          message: "User Already Exists In DB",
+          insertedId: null,
+        });
+      }
+      const result=await usersCollection.insertOne(userInfo);
+      res.send(result)
+    });
 
 
+    app.post("/tasks", async (req, res) => {
+      const data = req.body;
+      const result = await taskCollection.insertOne(data);
+      res.send(result);
+    });
 
-    app.delete("/tasks/:id",async(req,res)=>{
-        const id=req.params.id;
-        const query={_id: new ObjectId(id)}
-        const result=await taskCollection.deleteOne(query);
-        res.send(result);
-    })
+    app.get("/tasks", async (req, res) => {
+      const result = await taskCollection.find().toArray();
+      res.send(result);
+    });
 
+    app.get("/tasks/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await taskCollection.findOne(query);
+      res.send(result);
+    });
 
-    app.put("/tasks/:id",async(req,res)=>{
-        const id=req.params.id;
-        const {updatedTask}=req.body
-        const query={_id: new ObjectId(id)}
-        const updatedDoc={
-            $set:updatedTask
-        }
-        const result=await taskCollection.updateOne(query,updatedDoc);
-        res.send(result);
-    })
+    app.delete("/tasks/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await taskCollection.deleteOne(query);
+      res.send(result);
+    });
 
-
-    
+    app.put("/tasks/:id", async (req, res) => {
+      const id = req.params.id;
+      const { updatedTask } = req.body;
+      const query = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: updatedTask,
+      };
+      const result = await taskCollection.updateOne(query, updatedDoc);
+      res.send(result);
+    });
   } finally {
-   
   }
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("server is running properly");
+});
 
-
-
-
-
-
-
-app.get("/",(req,res)=>{
-    res.send("server is running properly");
-})
-
-
-app.listen(port,()=>{
-    console.log(`server is running on port ${port}`)
-})
+app.listen(port, () => {
+  console.log(`server is running on port ${port}`);
+});
